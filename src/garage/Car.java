06553520cf3f2c7809d0eab4f307146046b94fa5
Grigoriy Kosarev <grigoriykosarev@gmail.com>
2017-07-31 18:18:20 +0300

    сделал методы по Высадить всех пассажиров

--- src/garage/Car.java
+++ src/garage/Car.java
@@ -14,8 +14,8 @@ public class Car {
     private int passengerCapacity;        //пассажировместимость
     private int passengerQuantity;        //кол-во пасажиров внутри в данный момент
     private double speed;                 //текущая скорость
-    private CarWheel[] wheel;             //массив колес
-    private CarDoor[] door;               //массив дверей
+    private CarWheel[] wheels = new CarWheel[4]; //массив колес
+    private CarDoor[] doors = new CarDoor[4];               //массив дверей
 
     //Нет пустого конструктора. Так как есть поля в классе, которые нельзя изменять после создания объекта.
     // Например дата производства.
@@ -26,7 +26,7 @@ public class Car {
         accelerationTimeTo100 = 5.5;
         passengerCapacity = 5;
         passengerQuantity = 2;
-        speed = 50;
+        setSpeed(50);
     }
 
     //Конструктор с датой производства.
@@ -44,7 +44,7 @@ public class Car {
         this.accelerationTimeTo100 = accelerationTimeTo100;
         this.passengerCapacity = passengerCapacity;
         this.passengerQuantity = passengerQuantity;
-        this.speed = speed;
+        setSpeed(speed);
     }
 
     //Вывести в консоль данные об объекте (все поля и вычисленную максимальную скорость в зависимости от
@@ -60,4 +60,31 @@ public class Car {
         //private CarWheel[] wheel;             //массив колес
         //private CarDoor[] door;               //массив дверей
     }
+
+    //Изменить текущую скорость
+    public void setSpeed(double speed){
+        this.speed = speed;
+    }
+
+    //Посадить 1 пассажира в машину
+    public void setAddOnePassengerQuantity(){
+        if ((passengerQuantity + 1) <= passengerCapacity)
+            passengerQuantity++;
+        else
+            System.out.println("Невозможно посадить 1 пассажира. Превышает общее количество пасажыров.");
+    }
+
+    //Высадить 1 пассажира
+    public void setDelOnePassengerQuantity(){
+        if (passengerQuantity > 0)
+            passengerQuantity--;
+        else
+            System.out.println("Невозможно высадить 1 пассажира. Текущее количество 0.");
+    }
+
+    //Высадить всех пассажиров
+    public void setClearPassengerQuantity(){
+        passengerQuantity = 0;
+    }
+    
 }
