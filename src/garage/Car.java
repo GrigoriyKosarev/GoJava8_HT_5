b0e1b14b1eda5ccae289599193c4d92d9397fe6c
Grigoriy Kosarev <grigoriykosarev@gmail.com>
2017-07-31 16:27:15 +0300

    создание конструкторов класа Car

--- src/garage/Car.java
+++ src/garage/Car.java
@@ -1,8 +1,50 @@
 package garage;
 
+import java.util.Date;
+
 /**
  * Created by admin on 31.07.2017.
  */
 public class Car {
 
+    private Date dataProduce;             //дата производства (не изменна после создания объекта)
+    private String engineType;            //тип двигателя
+    private double maxSpeed;              //максимальная скорость машины (если она новая)
+    private double accelerationTimeTo100; //время разгона до 100км/ч
+    private int passengerCapacity;        //пассажировместимость
+    private int passengerQuantity;        //кол-во пасажиров внутри в данный момент
+    private double speed;                 //текущая скорость
+    private CarWheel[] wheel;             //массив колес
+    private CarDoor[] door;               //массив дверей
+
+    //Нет пустого конструктора. Так как есть поля в классе, которые нельзя изменять после создания объекта.
+    // Например дата производства.
+    Car(){
+        dataProduce = new Date(20170101);
+        engineType = "резкий";
+        maxSpeed = 200;
+        accelerationTimeTo100 = 5.5;
+        passengerCapacity = 5;
+        passengerQuantity = 2;
+        speed = 50;
+    }
+
+    //Конструктор с датой производства.
+    Car(Date dataProduce){
+        this.dataProduce = dataProduce;
+    }
+
+    //Конструктор со всеми полями, кроме массива колес и массива дверей.
+    Car(Date dataProduce, String engineType, double maxSpeed, double accelerationTimeTo100, int passengerCapacity,
+        int passengerQuantity, double speed){
+
+        this.dataProduce = dataProduce;
+        this.engineType = engineType;
+        this.maxSpeed = maxSpeed;
+        this.accelerationTimeTo100 = accelerationTimeTo100;
+        this.passengerCapacity = passengerCapacity;
+        this.passengerQuantity = passengerQuantity;
+        this.speed = speed;
+
+    }
 }
--- src/garage/Processor.java
+++ src/garage/Processor.java
@@ -25,6 +25,9 @@ public class Processor {
         wheel.setEraseOfPercentCondition(10);
         wheel.info();
 
+        //Клас Car
+        Car mustang = new Car();
+
     }
 
 }
